39241d457f1202a1dd9d211322c087313d6c64f7
jp3222 <juanpc.559@gmail.com>
2022-08-31 00:19:52 -0500

    Primer commit

---
+++ src/plyf_t1/Estilo4.java
@@ -0,0 +1,64 @@
+package plyf_t1;
+
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author jp
+ */
+public class Estilo4 {
+
+    public Estilo4() {
+
+    }
+
+    public void ventana() {
+        JFrame ventana = new JFrame("xd");
+        ventana.setSize(400, 400);
+        ventana.setLocationRelativeTo(ventana);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        vista(ventana);
+        ventana.setVisible(true);
+
+    }
+
+    public void vista(JFrame ventana) {
+        JPanel vista = new JPanel(new GridLayout(3, 1));
+        componentes(vista);
+        ventana.setContentPane(vista);
+    }
+
+    public void componentes(JPanel vista) {
+        JButton saludo = new JButton("Saludar");
+        vista.add(saludo, 0);
+
+        JButton saludo_nombre = new JButton("Saludo personal");
+        vista.add(saludo_nombre, 1);
+
+        JButton despedida = new JButton("Despedida");
+        vista.add(despedida, 2);
+
+        saludo.addActionListener(e -> evento(1));
+        saludo_nombre.addActionListener(e -> evento(2));
+        despedida.addActionListener(e -> evento(3));
+    }
+
+    public void evento(int i) {
+        if (i == 1) {
+            JOptionPane.showMessageDialog(null, "Hola Mundo");
+        } 
+        else if (i == 2) {
+            String nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
+            JOptionPane.showMessageDialog(null, "Hola " + nombre);
+        } 
+        else if (i > 2) {
+            JOptionPane.showMessageDialog(null, "Adios");
+            System.exit(0);
+        }
+    }
+
+}
